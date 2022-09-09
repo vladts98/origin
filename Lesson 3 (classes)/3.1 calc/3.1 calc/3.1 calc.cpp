d171c555b96602642f92bdab4a90379066ee84c4
Vladislav Tsvetkov <vaflik1998@gmail.com>
2022-09-09 23:14:47 +0300

    edit L3

--- Lesson 3 (classes)/3.1 calc/3.1 calc/3.1 calc.cpp
+++ Lesson 3 (classes)/3.1 calc/3.1 calc/3.1 calc.cpp
@@ -2,79 +2,73 @@
 
 class Calculator {
 public:
-    void set() {
-        double di = 0;
-        do {
-            std::cout << "Введите num1: ";
-            std::cin >> di;
-        } while (!set_num1(di));
+	bool set_num1(double num1) {
+		if (num1 == 0) {
+			std::cout << "Неверный ввод!" << std::endl;
+			return false;
+		}
+		else {
+			this->num1 = num1;
+			return true;
+		}
+	}
+	bool set_num2(double num2) {
+		if (num2 == 0) {
+			std::cout << "Неверный ввод!" << std::endl;
+			return false;
+		}
+		else {
+			this->num2 = num2;
+			return true;
+		}
+	}
 
-        do {
-            std::cout << "Введите num2: ";
-            std::cin >> di;
-        } while (!set_num2(di));
-
-    }
-
-    void print() {
-        std::cout << "num1 + num2 = " << add() << std::endl;
-        std::cout << "num1 - num2 = " << multiply() << std::endl;
-        std::cout << "num2 - num1 = " << subtract_1_2() << std::endl;
-        std::cout << "num1 * num2 = " << subtract_2_1() << std::endl;
-        std::cout << "num1 / num2 = " << divide_1_2() << std::endl;
-        std::cout << "num2 / num1 = " << divide_2_1() << std::endl;
-    }
+	double add() {
+		return num1 + num2;
+	}
+	double multiply() {
+		return num1 * num2;
+	}
+	double subtract_1_2() {
+		return num2 - num1;
+	}
+	double subtract_2_1() {
+		return (num1 - num2);
+	}
+	double divide_1_2() {
+		return (num1 / num2);
+	}
+	double divide_2_1() {
+		return (num2 / num1);
+	}
 private:
-    double num1;
-    double num2;
-
-    bool set_num1(double num1) {
-        if (num1 == 0) {
-            std::cout << "Неверный ввод!" << std::endl;
-            return false;
-        }
-        else {
-            this->num1 = num1;
-            return true;
-        }
-    }
-    bool set_num2(double num2) {
-        if (num2 == 0) {
-            std::cout << "Неверный ввод!" << std::endl;
-            return false;
-        }
-        else {
-            this->num2 = num2;
-            return true;
-        }
-    }
-
-    double add() {
-        return num1 + num2;
-    }
-    double multiply() {
-        return num1 * num2;
-    }
-    double subtract_1_2() {
-        return num2 - num1;
-    }
-    double subtract_2_1() {
-        return (num1 - num2);
-    }
-    double divide_1_2() {
-        return (num1 / num2);
-    }
-    double divide_2_1() {
-        return (num2 / num1);
-    }
+	double num1;
+	double num2;
 };
+
+void print(Calculator calc) {
+	std::cout << "num1 + num2 = " << calc.add() << std::endl;
+	std::cout << "num1 - num2 = " << calc.multiply() << std::endl;
+	std::cout << "num2 - num1 = " << calc.subtract_1_2() << std::endl;
+	std::cout << "num1 * num2 = " << calc.subtract_2_1() << std::endl;
+	std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+	std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+}
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    Calculator calc;
-    while (1) {
-        calc.set();
-        calc.print();
-    }
- }
+	setlocale(LC_ALL, "Russian");
+	Calculator calc;
+	while (1) {
+		double buff = 0;
+		do {
+			std::cout << "Введите num1: ";
+			std::cin >> buff;
+		} while (!calc.set_num1(buff));
+		do {
+			std::cout << "Введите num2: ";
+			std::cin >> buff;
+		} while (!calc.set_num2(buff));
+		print(calc);
+	}
+}
--- Lesson 3 (classes)/3.2 count/3.2 count/3.2 count.cpp
+++ Lesson 3 (classes)/3.2 count/3.2 count/3.2 count.cpp
@@ -4,48 +4,54 @@
 class Counter {
 public:
     Counter() {
-        std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-        std::string answer;
-        std::cin >> answer;
-        if (answer == "нет") {
-        } else if (answer == "да") {
-            std::cout << "Введите начальное значение счётчика: ";
-            std::cin >> count;
-        }
     }
-    bool make(std::string& pull) {
-        if (pull == "+") {
-            count++;
-            return true;
-        } else if (pull == "-") {
-            count--;
-            return true;
-        }
-        else if (pull == "=") {
-            std::cout << count << std::endl;
-        }
-        else if (pull == "x" || pull == "х") {
-            std::cout << "До свидания! ";
-            return false;
-        }
-        else {
-            std::cout << "bad";
-            return false;
-        }
+    Counter(int& count) {
+        this->count = count;
+    }
+    void plus() {
+        count++;
+    }
+    void minus() {
+        count--;
+    }
+    void view() {
+        std::cout << count <<std::endl;
     }
 
 private:
     int count = 1;
-    
 };
 int main()
 {
     SetConsoleCP(1251);
-    SetConsoleOutputCP(1251); 
-    Counter c;
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::string answer;
+    std::cin >> answer;
+
+    int count = 1;
+    if (answer == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> count;
+    }
+    else if (answer == "нет") {
+    }
+    Counter c(count);
+
     std::string pull;
     do {
         std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
         std::cin >> pull;
-    } while (c.make(pull));
+        if (pull == "+") {
+            c.plus();
+        }
+        else if (pull == "-") {
+            c.minus();
+        }
+        else if (pull == "=") {
+            c.view();
+        }
+    } while (pull != "х");
+    std::cout << "До свидания! ";
 }
